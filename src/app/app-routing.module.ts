b44ed0f509adffd9da248079fb7fab75139e4686
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-10-06 23:29:41 +0200

    Added navigation styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './main/main.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
   { path: 'main', component: MainComponent, data: { state: 'main' } },
   { path: 'projects', component: ProjectsComponent, data: { state: 'projects' } },
   { path: 'contact', component: ContactComponent, data: { state: 'contact' } },
-  { path: '**', component: MainComponent }
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
