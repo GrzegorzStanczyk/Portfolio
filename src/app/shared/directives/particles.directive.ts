609fbfcdda7bf986376e26429d5340d1017cbe92
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-10-08 22:17:00 +0200

    Added canvas resize with rxjs trottleTime

--- src/app/shared/directives/particles.directive.ts
+++ src/app/shared/directives/particles.directive.ts
@@ -1,32 +1,36 @@
 import { Directive, ElementRef, OnInit, AfterViewInit, HostListener, Input } from '@angular/core';
 
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/throttleTime';
+
 @Directive({
   selector: '[appParticles]'
 })
+
 export class ParticlesDirective implements OnInit, AfterViewInit {
-  @HostListener('window:resize')
-  resize(event) {
-    this.setCanvasSize();
-  }
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    console.log(event.target.innerWidth);
-  }
+  private resizeEvent$: Subject<MouseEvent> = new Subject<MouseEvent>();
+
   constructor(private el: ElementRef) { }
 
-  ngOnInit() {
+  setCanvasSize(ev?): void {
+    if(ev) console.log(ev.target.innerWidth);
     
-  }
-
-  setCanvasSize(): void {
     const canvasEl: HTMLCanvasElement = this.el.nativeElement;
     let ctx: CanvasRenderingContext2D = canvasEl.getContext('2d');
     canvasEl.width = window.innerWidth/2;
     canvasEl.height = window.innerHeight;
   }
 
+  ngOnInit() {
+    this.resizeEvent$.throttleTime(200).subscribe(event => this.setCanvasSize(event));
+  }
+
   ngAfterViewInit() {
     this.setCanvasSize();
   }
 
+  @HostListener('window:resize', ['$event'])
+  resize(event) {
+    this.resizeEvent$.next(event);
+  }
 }
