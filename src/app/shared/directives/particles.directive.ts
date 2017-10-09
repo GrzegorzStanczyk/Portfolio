b1f90dbb076817b74c163783b3af1c63a5558eeb
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-10-09 22:45:53 +0200

    Particle drawing added

--- src/app/shared/directives/particles.directive.ts
+++ src/app/shared/directives/particles.directive.ts
@@ -9,28 +9,135 @@ import 'rxjs/add/operator/throttleTime';
 
 export class ParticlesDirective implements OnInit, AfterViewInit {
   private resizeEvent$: Subject<MouseEvent> = new Subject<MouseEvent>();
+  private canvasEl: HTMLCanvasElement = this.el.nativeElement;
+  private ctx: CanvasRenderingContext2D = this.canvasEl.getContext('2d');
+  private numberOfParticles: number = 5;
+  private particlesArray: Array<any> = [];
+  private clearTimeout: Array<any> = [];
+  private colorsArray: Array<string> = [
+      "#00d1cb", "#ff4699", "#fbb63a", "#b3dc5b", "#ffe100"
+  ];
+
+  myReq = requestAnimationFrame(() => this.loop());
+  pause = null;
 
   constructor(private el: ElementRef) { }
 
-  setCanvasSize(ev?): void {
-    if(ev) console.log(ev.target.innerWidth);
+  setCanvasSize(): void {
+    this.canvasEl.width = window.innerWidth/2;
+    this.canvasEl.height = window.innerHeight;
+  }
+
+  clearCanvas(): void {
+    this.ctx.clearRect(0, 0, window.innerWidth/2, window.innerHeight);
+  }
+
+  createParticles(): void {
+    // if(this.particlesArray.length > 100) {this.particlesArray.shift();}
+    if(this.particlesArray.length > 100) return 
     
-    const canvasEl: HTMLCanvasElement = this.el.nativeElement;
-    let ctx: CanvasRenderingContext2D = canvasEl.getContext('2d');
-    canvasEl.width = window.innerWidth/2;
-    canvasEl.height = window.innerHeight;
+    let p = {
+      x: window.innerWidth/2,
+      y: Math.random() * window.innerHeight,
+      r: Math.floor(Math.random() * 10) + 1,
+      color: this.colorsArray[Math.floor(Math.random() * this.colorsArray.length)]
+    }
+    this.particlesArray.push(p)
+  }
+
+  animateParticles(): any {
+    this.particlesArray.forEach(p => {
+      p.x -= 0.1;
+      p.y += (Math.random() - 0.5)*0.2;
+    })
+  }
+
+
+  drawParticles(): void {
+    // for(let i = 1; i < this.numberOfParticles; i++) {
+      // this.clearTimeout.push(
+        // setTimeout(()=> {
+          this.particlesArray.forEach(p => {
+            this.ctx.beginPath();
+            this.ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2, false);
+            this.ctx.fillStyle = p.color;
+            this.ctx.fill();
+          })
+        // }, i * 2000)
+      // )
+    // }
+  }
+
+
+
+  // drawParticles(): void {
+  //   for(let i = 1; i < this.numberOfParticles; i++) {
+  //     // let x = Math.random() * window.innerWidth;
+  //     // let p = {
+  //     //   x: window.innerWidth/2,
+  //     //   y: Math.random() * window.innerHeight,
+  //     //   r: Math.floor(Math.random() * 10) + 1
+  //     // }
+
+  //     this.clearTimeout.push(
+  //       setTimeout(()=> {
+
+  //         let p = {
+  //           x: window.innerWidth/2,
+  //           y: Math.random() * window.innerHeight,
+  //           r: Math.floor(Math.random() * 10) + 1
+  //         }
+
+  //         this.particlesArray.push(p)
+
+  //         this.ctx.beginPath();
+  //         this.ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2, false);
+  //         this.ctx.fillStyle = this.colorsArray[Math.floor(Math.random() * this.colorsArray.length)];
+  //         this.ctx.fill();
+
+  //   this.animateParticles();
+          
+  //       }, i * 2000)
+  //     )
+  //   }
+  // }
+
+  cancelDraw(): void {
+    this.clearTimeout.forEach(particle => clearTimeout(particle));
   }
 
   ngOnInit() {
-    this.resizeEvent$.throttleTime(200).subscribe(event => this.setCanvasSize(event));
+    this.resizeEvent$.throttleTime(200)
+    .subscribe(event => {
+      // this.animateParticles()
+      // this.cancelDraw();
+      // this.clearCanvas();
+      // this.pause = true;
+      cancelAnimationFrame(this.myReq)
+      this.setCanvasSize();
+      // this.drawParticles();
+      // this.pause = undefined;
+      this.particlesArray = [];
+      this.loop();
+    });
+  }
+
+  loop() {
+    if(this.pause) return;
+    this.clearCanvas();
+    this.createParticles();
+    this.animateParticles();
+    this.drawParticles();
+    this.myReq = requestAnimationFrame(() => this.loop());
   }
 
   ngAfterViewInit() {
     this.setCanvasSize();
+    this.loop();
   }
 
-  @HostListener('window:resize', ['$event'])
-  resize(event) {
-    this.resizeEvent$.next(event);
+  @HostListener('window:resize')
+  resize() {
+    this.resizeEvent$.next();
   }
 }
