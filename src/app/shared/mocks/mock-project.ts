281ab759fa538c3fd452ed139b06c812500785b1
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-10-25 23:41:03 +0200

    Added path to projects mock

--- src/app/shared/mocks/mock-project.ts
+++ src/app/shared/mocks/mock-project.ts
@@ -1,7 +1,21 @@
 import { Project } from '@app/shared';
 
 export const PROJECTS: Project[] = [
-    { title: 'Key Viewer', tech: ['firebase', 'Angular', 'Rest'], desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsa non nostrum possimus molestias nemo reprehenderit soluta. Beatae corporis inventore vitae voluptatem, non animi id tenetur ipsum doloribus commodi porro!', live: '#', git: '#'},
-    { title: 'title', tech: ['firebase'], desc: 'lorem', live: 'https://', git: 'https://'},
-    { title: 'title', tech: ['firebase'], desc: 'lorem', live: 'https://', git: 'https://'}
+    { 
+        path: 'key-viewer',
+        title: 'Key Viewer', 
+        tech: ['firebase', 'Angular', 'Rest'], 
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsa non nostrum possimus molestias nemo reprehenderit soluta. Beatae corporis inventore vitae voluptatem, non animi id tenetur ipsum doloribus commodi porro!', 
+        live: '#', 
+        git: '#'
+    },
+    { 
+        path: 'dottipelotti',
+        title: 'dottipelotti', 
+        tech: ['firebase'], 
+        desc: 'dottipelotti', 
+        live: 'https://', 
+        git: 'https://'
+    }
 ];
+
