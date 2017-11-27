575be7e8540085593de5e5a4b66896617beefc52
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-11-27 20:30:19 +0100

    Added ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
@@ -49,7 +49,8 @@ export class MyHammerConfig extends HammerGestureConfig  {
     BrowserModule.withServerTransition({appId: 'universal-portfolio'}),
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ResizeService,
     StorageService,
