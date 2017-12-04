eb6853fdb2cd21d8d7daf635d0331ec646617c5a
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-12-04 12:10:49 +0100

    Added particles in small screen

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,25 +2,19 @@ import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 
 import { Router } from '@angular/router';
 
-import { ResizeService } from '@app/shared';
 import { NavigateService } from '@app/shared';
 
-import { Subscription } from 'rxjs/Subscription';
-import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit, OnDestroy {
+export class MainComponent implements OnInit {
   public message: string = 'Inspirations are everywhere';
-  public showCanvas: boolean = true;
-  private resizeSubscription: Subscription;
 
   constructor(
     private router: Router,
-    private resizeService: ResizeService,
     private navigateService: NavigateService) { }
 
   @HostListener('document:keydown.ArrowRight')
@@ -29,18 +23,5 @@ export class MainComponent implements OnInit, OnDestroy {
     this.navigateService.navigateToProjects();
   }
 
-  private canvasState(event = window): boolean {
-    return event.innerWidth <= 700 ? true : false;
-  }
-
-  ngOnInit() {
-    this.showCanvas = this.canvasState();
-    this.resizeSubscription = this.resizeService.resizeSubject$
-    .debounceTime(200)
-    .subscribe(event => this.showCanvas = this.canvasState(event));
-  }
-
-  ngOnDestroy() {
-    this.resizeSubscription.unsubscribe();
-  }
+  ngOnInit() {}
 }
